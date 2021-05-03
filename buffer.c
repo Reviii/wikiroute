6973b473576e6a9f4a96ea159a65d69dd30258e3
Reviii <Reviii@users.noreply.github.com>
2021-05-03 21:24:21 +0200

    buffer: Inline bufferAdd

--- buffer.c
+++ buffer.c
@@ -2,16 +2,6 @@
 #include <stdlib.h>
 
 #include "buffer.h"
-char * bufferAdd(struct buffer * buf, size_t amount) {
-    size_t res = buf->used;
-    while (buf->used+amount>buf->_size) {
-        buf->_size *= 2;
-        buf->content = realloc(buf->content, buf->_size);
-        assert(buf->content);
-    }
-    buf->used += amount;
-    return (buf->content + res);
-}
 struct buffer bufferCreate() {
     struct buffer res = {NULL, 0, 1024};
     res.content = malloc(1024);
--- buffer.h
+++ buffer.h
@@ -1,3 +1,4 @@
+#include <assert.h>
 // buffer is an automatically resizing array of chars
 struct buffer {
     char * content;
@@ -5,9 +6,17 @@ struct buffer {
     size_t _size;
 };
 struct buffer bufferCreate();
-char * bufferAdd(struct buffer * buf, size_t amount);
 void bufferCompact(struct buffer * buf);
-
+inline char * bufferAdd(struct buffer * buf, size_t amount) {
+    size_t res = buf->used;
+    while (buf->used+amount>buf->_size) {
+        buf->_size *= 2;
+        buf->content = realloc(buf->content, buf->_size);
+        assert(buf->content);
+    }
+    buf->used += amount;
+    return (buf->content + res);
+}
 
 /* examples
 1. copy string to buffer and print it
