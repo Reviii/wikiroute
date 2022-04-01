4d4eec61f3b7e4ef80780f7b591b2e7da750633a
Reviii <Reviii@users.noreply.github.com>
2022-04-01 20:29:49 +0200

    nodeutils: Also use correct types in header uint32_t was used instead of nodeRef

--- nodeutils.h
+++ nodeutils.h
@@ -1,9 +1,9 @@
-uint32_t * getNodeOffsets(char * nodeData, size_t nodeDataLength, size_t * nodeCount);
+nodeRef * getNodeOffsets(char * nodeData, size_t nodeDataLength, size_t * nodeCount);
 size_t * getTitleOffsets(FILE * f, size_t * titleCount);
 char * getTitle(FILE * titles, size_t * titleOffsets, size_t id);
-char * nodeOffsetToTitle(FILE * titles, uint32_t * nodeOffsets, size_t * titleOffsets, size_t nodeCount, uint32_t nodeOffset);
+char * nodeOffsetToTitle(FILE * titles, nodeRef * nodeOffsets, size_t * titleOffsets, size_t nodeCount, nodeRef nodeOffset);
 void normalizeTitle(char * title);
-uint32_t titleToNodeOffset(FILE * titles, uint32_t * nodeOffsets, size_t * titleOffsets, size_t nodeCount, char * title);
-static inline struct wikiNode * getNode(char * nodeData, uint32_t offset) {
+nodeRef titleToNodeOffset(FILE * titles, nodeRef * nodeOffsets, size_t * titleOffsets, size_t nodeCount, char * title);
+static inline struct wikiNode * getNode(char * nodeData, nodeRef offset) {
     return (struct wikiNode *) (nodeData + offset);
 }
